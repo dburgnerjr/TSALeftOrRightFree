15450a28ebf690a2060014636283962ef910f870
Daniel Burgner <dburgnerjr@yahoo.com>
2019-10-07 21:35:44 -0500

    D2/D3 Migrated to AndroidX
    D2/D3 of #100DaysOfCode #2019Part2 challenge.  Migrated
    #TSARandomizerFree version to #AndroidX.  #AndroidDev #BuiltWithUdacity

--- app/src/main/java/com/danielburgnerjr/tsaleftorrightfree/MenuActivity.java
+++ app/src/main/java/com/danielburgnerjr/tsaleftorrightfree/MenuActivity.java
@@ -4,10 +4,9 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
